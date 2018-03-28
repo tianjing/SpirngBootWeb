cbc0172a73e4ddf5502dff417cb1e01c338d216e
田径 <tian___jing@hotmail.com>
2018-03-28 14:41:46 +0800

    添加自定义定时任务mapper

--- src/main/java/tgtools/spirngbootweb/config/QuartzConfigration.java
+++ src/main/java/tgtools/spirngbootweb/config/QuartzConfigration.java
@@ -1,8 +1,17 @@
 package tgtools.spirngbootweb.config;
 
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import tgtools.util.ReflectionUtil;
+
+import javax.sql.DataSource;
 
 /**
  * 访问地址如：http://ip:port/${context-path}/quartz/explorer/manage/resource/quartz.html
@@ -16,5 +25,19 @@ public class QuartzConfigration  extends tgtools.quartz.explorer.config.QuartzCo
 //    @Override
 //    protected String getDataBaseType() {
 //        return "dm6";
+//    }
+
+//    @Bean
+//    @Override
+//    public SqlSessionFactory quartzSqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+//
+//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+//        factoryBean.setDataSource(dataSource);
+//        org.apache.ibatis.session.Configuration config = new org.apache.ibatis.session.Configuration();
+//        config.setMapUnderscoreToCamelCase(true);
+//        factoryBean.setConfiguration(config);
+//        factoryBean.setMapperLocations(new Resource[]{new UrlResource(ReflectionUtil.getResource("config/TaskMapper_dm6.xml"))});
+//        return factoryBean.getObject();
+//
 //    }
 }
