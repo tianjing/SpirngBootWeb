611e324a9294837733353f85dbd70ab05ca9cb0d
田径 <tian___jing@hotmail.com>
2018-04-08 17:39:36 +0800

    更新pom

--- src/main/java/tgtools/spirngbootweb/demo/websocket/ClientWebSocketHandler.java
+++ src/main/java/tgtools/spirngbootweb/demo/websocket/ClientWebSocketHandler.java
@@ -1,6 +1,8 @@
 package tgtools.spirngbootweb.demo.websocket;
 
+import org.apache.shiro.mgt.SecurityManager;
 import org.springframework.stereotype.Controller;
+import tgtools.web.develop.service.UserService;
 import tgtools.web.develop.websocket.AbstractSingleWebSocketHandler;
 
 /**
@@ -16,6 +18,16 @@ public class ClientWebSocketHandler extends AbstractSingleWebSocketHandler {
     @Override
     protected String getCommandType() {
         return "websocket";
+    }
+
+    @Override
+    protected SecurityManager getSecurityManager() {
+        return tgtools.web.platform.Platform.getBeanFactory().getBean(SecurityManager.class);
+    }
+
+    @Override
+    protected UserService getUserService() {
+        return tgtools.web.platform.Platform.getBeanFactory().getBean(UserService.class);
     }
 
     @Override
