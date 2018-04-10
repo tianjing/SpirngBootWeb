7d8968e7ef10e2a6c2e220fb4e4478ae5bc417ee
田径 <tian___jing@hotmail.com>
2018-04-10 15:26:50 +0800

    更新

--- src/main/java/tgtools/spirngbootweb/demo/websocket/ClientWebSocketHandler.java
+++ src/main/java/tgtools/spirngbootweb/demo/websocket/ClientWebSocketHandler.java
@@ -15,26 +15,46 @@ import tgtools.web.develop.websocket.AbstractSingleWebSocketHandler;
 public class ClientWebSocketHandler extends AbstractSingleWebSocketHandler {
 
 
+    /**
+     * 命令类型 用于加载 同类型的 CommandImpl
+     * @return
+     */
     @Override
     protected String getCommandType() {
         return "websocket";
     }
 
+    /**
+     * 认证管理器 模拟登录用到
+     * @return
+     */
     @Override
     protected SecurityManager getSecurityManager() {
         return tgtools.web.platform.Platform.getBeanFactory().getBean(SecurityManager.class);
     }
 
+    /**
+     * 用户认证服务
+     * @return
+     */
     @Override
     protected UserService getUserService() {
         return tgtools.web.platform.Platform.getBeanFactory().getBean(UserService.class);
     }
 
+    /**
+     * 匹配 ServletName 参看 TgtoolsConfig 中 servletName 对应
+     * @return
+     */
     @Override
     public String getServletName() {
         return "rest";
     }
 
+    /**
+     * 自定义 websocket url（/{ContextPath}/{ServletUrl}/socket）
+     * @return
+     */
     @Override
     public String getUrl() {
         return "/websocket";
